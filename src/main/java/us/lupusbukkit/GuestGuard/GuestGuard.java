64ce0102dc321660c9c519fc5b3335d4604cb231
ludo0777 <peter.wathall@gmail.com>
2012-04-13 11:57:01 +0100

    Added StopDrop
    Added stopping dropping items for creative (configurable) and for
    anyone without permission (guestguard.drop)

--- src/main/java/us/lupusbukkit/GuestGuard/GuestGuard.java
+++ src/main/java/us/lupusbukkit/GuestGuard/GuestGuard.java
@@ -1,17 +1,24 @@
 package us.lupusbukkit.GuestGuard;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
  
 public class GuestGuard extends JavaPlugin implements Listener {
     boolean stopChat = this.getConfig().getBoolean("stopChat");
     boolean stopPlace = this.getConfig().getBoolean("stopPlace");
     boolean stopBreak = this.getConfig().getBoolean("stopBreak");
+    boolean stopPickup = this.getConfig().getBoolean("stopPickup");
+    boolean stopDrop = this.getConfig().getBoolean("stopDrop");
+    boolean stopDropCreative = this.getConfig().getBoolean("stopDropCreative");
+    
+    
     @Override
     public void onDisable(){
        
@@ -50,6 +57,23 @@ public class GuestGuard extends JavaPlugin implements Listener {
         if(!player.hasPermission("guestguard.build")) {
         event.setCancelled(true);
         player.sendMessage(ChatColor.RED+"You cannot destroy blocks!");
+        }
+        }
+    }
+    
+    @EventHandler
+    public void StopDrop(PlayerDropItemEvent event){
+        Player player = event.getPlayer();
+        if(stopDropCreative){
+            if(player.getGameMode() == GameMode.CREATIVE){
+                event.setCancelled(true);
+                player.sendMessage(ChatColor.RED+"You cannot drop items!");
+            }
+        }
+        if(stopDrop){
+        if(!player.hasPermission("guestguard.drop")) {
+          event.setCancelled(true);
+          player.sendMessage(ChatColor.RED+"You cannot drop items!"); 
         }
         }
     }
